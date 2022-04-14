43da4b067cc925b4040e95d6f15a75a922101b1a
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-04-14 12:12:39 +0300

    Custom Alert Dialog

--- app/src/main/java/com/metehanbolat/alertcustomdialogcompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/alertcustomdialogcompose/MainActivity.kt
@@ -3,12 +3,20 @@ package com.metehanbolat.alertcustomdialogcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -85,4 +93,77 @@ fun DefaultAlertDialog(
             }
         }
     )
+}
+
+@Composable
+fun CustomAlertDialog(
+    onDismiss: emptyReturnUnit,
+    onNegativeClick: emptyReturnUnit,
+    onPositiveClick: emptyReturnUnit
+) {
+    Dialog(
+        onDismissRequest = onDismiss
+    ) {
+        val color = Color(0xFF4DB64C)
+
+        Card(
+            elevation = 8.dp,
+            shape = RoundedCornerShape(12.dp)
+        ) {
+            Column {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(160.dp)
+                        .background(color)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_launcher_background),
+                        contentDescription = stringResource(id = R.string.alert_dialog_icon_description),
+                        modifier = Modifier
+                            .graphicsLayer(
+                                scaleX = 1.2f,
+                                scaleY = 1.2f
+                            )
+                            .align(Alignment.Center)
+                    )
+                }
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                        Text(text = stringResource(id = R.string.custom_dialog_text))
+                    }
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.not_now),
+                        color = color,
+                        modifier = Modifier
+                            .clickable(
+                                interactionSource = MutableInteractionSource(),
+                                indication = rememberRipple(color = Color.DarkGray),
+                                onClick = onNegativeClick
+                            )
+                            .padding(8.dp)
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(
+                        text = stringResource(id = R.string._continue),
+                        color = color,
+                        modifier = Modifier
+                            .clickable(
+                                interactionSource = MutableInteractionSource(),
+                                indication = rememberRipple(color = Color.DarkGray),
+                                onClick = onPositiveClick
+                            )
+                            .padding(8.dp)
+                    )
+                }
+            }
+        }
+    }
 }
